6588f9605fbad970213ff2af55ddd8ff7b9217c1
Takamasa Sugiura <takamasas2008@gmail.com>
2023-01-26 22:20:05 +0900

    オプションを追加

--- server/aspnet/RabbitLifeGame/RabbitLifeGame/Pages/Life.cshtml.cs
+++ server/aspnet/RabbitLifeGame/RabbitLifeGame/Pages/Life.cshtml.cs
@@ -8,6 +8,10 @@ public class LifeModel : PageModel
     private readonly ILogger<LifeModel> _logger;
 
     public string Bg { get; private set; } = string.Empty;
+    public bool NoRabbit { get; private set; } = false;
+
+    public int[] BirthCondition { get; private set; } = new int[] { 3 };
+    public int[] LiveCondition { get; private set; } = new int[] { 2, 3 };
 
     public LifeModel(ILogger<LifeModel> logger)
     {
@@ -17,6 +21,22 @@ public class LifeModel : PageModel
     public void OnGet(GetRequest getRequest)
     {
         Bg = getRequest.Bg;
+        NoRabbit = getRequest.NoRabbit;
+        if (!string.IsNullOrEmpty(getRequest.Rule))
+        {
+            var splited = getRequest.Rule.Split("_");
+            try
+            {
+                if (splited.Length == 2)
+                {
+                    var birthCondition = splited[0].Select(x => int.Parse(x.ToString())).ToArray();
+                    var liveCondition = splited[1].Select(x => int.Parse(x.ToString())).ToArray();
+                    BirthCondition = birthCondition;
+                    LiveCondition = liveCondition;
+                }
+            }
+            catch { }
+        }
     }
 
     [BindProperties]
@@ -24,5 +44,6 @@ public class LifeModel : PageModel
     {
         public string Rule { get; set; } = string.Empty;
         public string Bg { get; set; } = string.Empty;
+        public bool NoRabbit { get; set; } = false;
     }
 }
