c2aec81ed6b8f05fbc1f32b6b83a9d04cc75758f
Takamasa Sugiura <takmasas2008@gmail.com>
2023-01-30 21:11:38 +0900

    commit

--- server/aspnet/RabbitLifeGame/RabbitLifeGame/Pages/Game1.cshtml.cs
+++ server/aspnet/RabbitLifeGame/RabbitLifeGame/Pages/Game1.cshtml.cs
@@ -1,12 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace RabbitLifeGame.Pages
+namespace RabbitLifeGame.Pages;
+
+public class Game1Model : PageModel
 {
-    public class Game1Model : PageModel
+    public int Size { get; private set; } = 3;
+
+    public void OnGet(GetRequest request)
     {
-        public void OnGet()
+        if (request.Size is >= 3 and <= 10)
         {
+            Size = request.Size;
         }
+    }
+
+    [BindProperties]
+    public class GetRequest
+    {
+        public string Bg { get; set; } = string.Empty;
+        public string Fg { get; set; } = "white";
+        public int Size { get; set; } = 3;
     }
 }
