86b3467af5df2abbfdcd14dca31c7ff735274bfd
Takamasa Sugiura <takmasas2008@gmail.com>
2023-03-19 09:12:21 +0900

    commit

---
+++ server/aspnet/RabbitLifeGame/RabbitLifeGame/Hubs/MouseTraceHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace RabbitLifeGame.Hubs;
+
+public class MouseTraceHub : Hub
+{
+	public async Task MouseMoveCommand(Request request)
+	{
+		var ret = new Response { X = request.X, Y = request.Y };
+		await Clients.All.SendAsync("MouseMoveMessage", ret);
+	}
+
+	private class Response
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+	}
+
+	public class Request
+	{
+		public int X { get; set; }
+		public int Y { get; set; }
+	}
+}
---
+++ server/aspnet/RabbitLifeGame/RabbitLifeGame/Pages/MouseTrace.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RabbitLifeGame.Pages
+{
+    public class MouseTraceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
